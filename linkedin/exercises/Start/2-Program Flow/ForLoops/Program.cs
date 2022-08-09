ed1b8f3904586bc3321c3cfbd5954eceb33ef237
Tony Kieling <tony.kieling@gmail.com>
2022-08-08 17:18:09 -0700

    finished 1-Overview + 2-Program Flow

---
+++ linkedin/exercises/Start/2-Program Flow/ForLoops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVal = 15;
+            int[] nums = new int[] {3, 14, 15, 92, 6};
+            string str = "The quick brown fox jumps over the lazy dog O";
+            // string str = "TesT";
+
+            // TODO: the basic for loop
+            // Console.WriteLine("The basic for loop:");
+            // for (int i = 0; i < myVal; i++) {
+            //     Console.WriteLine("current i is: {0}", i);
+            // }
+
+
+            // Console.WriteLine();
+
+            // TODO: the foreach-in loop can be used to iterate over sequences
+            // Console.WriteLine("The foreach loop:");
+            // Console.WriteLine("nums array: " + "[{0}]", string.Join(", ", nums));
+            // foreach (int i in nums) {
+            //     Console.WriteLine("   - i is {0}" + i);
+            // }
+
+
+            // TODO: count the number of o's in the string
+            int count = 0;
+            foreach (char c in str) {
+                if (c == 'o' || c == 'O') count++;
+            }
+            Console.WriteLine(" letter o OR O appears {0} times", count);
+
+        }
+    }
+}
---
+++ linkedin/exercises/Start/2-Program Flow/WhileLoops/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "";
+
+            // TODO: basic while loop executes while the gate condition is true
+            Console.WriteLine("Basic while() loop:");
+
+            while(inputStr != "exit") {
+                inputStr = Console.ReadLine();
+                // if (inputStr == "exit") break;
+
+                Console.WriteLine(" you entered: {0}", inputStr);
+            }
+
+            // Console.WriteLine("byyye");
+
+            // TODO: the do-while loop always executes at least one time
+            // Console.WriteLine("The  do-while() loop:");
+            // do {
+            //     Console.WriteLine(" \ntype something: ");
+            //     inputStr = Console.ReadLine();
+            //     Console.WriteLine(" you entered {0}", inputStr );
+            // } while (inputStr != "exit");
+            // Console.WriteLine(" CY");
+        }
+    }
+}
