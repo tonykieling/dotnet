5cac445c4d3525c44272243061ad293b574708de
Tony Kieling <tony.kieling@gmail.com>
2022-08-19 16:19:31 -0700

    :3- chapter 5(Regex) and the whole course are done

---
+++ linkedin/3-c#_.net-essentials/exercises/Start/5-Regex/Finding/Program.cs
@@ -0,0 +1,37 @@
+﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
+// Example file for using Regex to find patterns
+
+using System.Text.RegularExpressions;
+
+string teststr1 = "The quick brown Fox jumps over the lazy Dog";
+string teststr2 = "the quick brown fox jumps over the lazy dog";
+
+// TODO: The IsMatch function is used to determine if the content of a string
+// results in a match with the given Regex
+Regex capWords = new Regex(@"[A-Z]\w+");
+// Console.WriteLine(capWords.IsMatch(teststr1));
+// Console.WriteLine(capWords.IsMatch(teststr2));
+
+// TODO: The RegexOptions argument can be used to perform
+// case-insensitive searches
+// Regex noCase = new Regex(@"fox", RegexOptions.IgnoreCase);
+// Console.WriteLine(noCase.IsMatch(teststr1));
+// Console.WriteLine(noCase.IsMatch(teststr2));
+
+// Use the Match and Matches methods to get information about
+// specific Regex matches for a given pattern
+// Match m = capWords.Match(teststr1);
+// while (m.Success) {
+//   Console.WriteLine($"'{m.Value}' found at position {m.Index}");
+//   m = m.NextMatch();
+// }
+
+// TODO: The Match method returns a single match at a time
+MatchCollection mc = capWords.Matches(teststr1);
+Console.WriteLine($"Found {mc.Count} matches");
+foreach (Match match in mc) {
+  Console.WriteLine($"'{match.Value}' found at position {match.Index}");
+}
+
+// TODO: The Matches method returns a collection of Match objects
+
