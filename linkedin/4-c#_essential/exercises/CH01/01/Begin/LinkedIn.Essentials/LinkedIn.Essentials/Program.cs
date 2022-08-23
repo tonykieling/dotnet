0b88303adf578ee71e5af5e583dc35f1dc6192d9
Tony Kieling <tony.kieling@gmail.com>
2022-08-22 17:40:35 -0700

    4:- chapter 1 is dobem doing chapter 2 + iVS nstalation on Windows

---
+++ linkedin/4-c#_essential/exercises/CH01/01/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedIn.Essentials;
+
+Person me = new Person();
+me.FirstName = "Matt";
+me.LastName = "Milner";
+
+Console.WriteLine("\n\n X Hello from, {0} {1}, to you!!!X", 
+    me.FirstName, me.LastName);
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ linkedin/4-c#_essential/exercises/CH02/02/Begin/LinkedIn.Essentials/LinkedIn.Essentials/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedIn.Essentials;
+
+Console.WriteLine("Hello World");
+Employee e = new Employee("Tony", "K+", 8);
+Console.WriteLine($"\n\tName is: {e.FirstName} {e.LastName}, Id is: {e.Id}");
